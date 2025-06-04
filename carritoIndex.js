db09ee9d3c216897b5e19b17d5ea05bcdf1ccb5a
Federico Callegari <callegarifede3@gmail.com>
2025-06-04 17:21:10 -0300

    Add carritoIdex

---
+++ carritoIndex.js
@@ -0,0 +1,8 @@
+import { productos } from "./stock";
+
+const contenedorCarrito = document.getElementById("carrito-contenedor");
+const cantidadSteps = 2;
+let stepActual = 1;
+
+let carritoDeCompras = [];
+
