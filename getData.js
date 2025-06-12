537f78d631f8bc91e7173e99c7e58451731b11ff
Federico Callegari <callegarifede3@gmail.com>
2025-06-12 17:52:53 -0300

    Fix bug deploy

--- getData.js
+++ getData.js
@@ -2,6 +2,7 @@ const getData = async ()=>{
     try{
         const response = await fetch("stock.json");
         const data = await response.json();
+        console.log(data);
         
         return data;
     } catch (error){
